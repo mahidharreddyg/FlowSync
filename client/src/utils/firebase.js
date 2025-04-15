98a75c76367512e6d2d560365d09b4b80d6b9a27
Mahidhar Reddy G <mahidhar.reddy2003@gmail.com>
2025-04-15 23:47:39 +0530

    Adds backend

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,12 +1,18 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: "taskmanager-557d7.firebaseapp.com",
-  projectId: "taskmanager-557d7",
-  storageBucket: "taskmanager-557d7.appspot.com",
-  messagingSenderId: "824261215224",
-  appId: "1:824261215224:web:ba24631a12b4bf9d5e8c0d",
+  authDomain: "taskmanager-4f4b4.firebaseapp.com",
+  projectId: "taskmanager-4f4b4",
+  storageBucket: "taskmanager-4f4b4.firebasestorage.app",
+  messagingSenderId: "811829037186",
+  appId: "1:811829037186:web:34291318296710b1810c9b"
 };
 
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// ✅ Export app so it can be imported elsewhere
+export { app };
